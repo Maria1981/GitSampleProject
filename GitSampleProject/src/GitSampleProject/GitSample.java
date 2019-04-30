d23ed23b6f50e35896cc5c66baf34ac7699c80cc
Selva Rani <selva_rani1981@yahoo.com>
2019-04-30 22:26:33 +0530

    Committed the GitSampleProject

---
+++ GitSampleProject/src/GitSampleProject/GitSample.java
@@ -0,0 +1,11 @@
+package GitSampleProject;
+
+public class GitSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Frist Project about Git & GitHub");
+
+	}
+
+}
