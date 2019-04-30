dc13e3607a4625126e7b6909a1dd219693afa29c
Selva Rani <selva_rani1981@yahoo.com>
2019-04-30 22:33:55 +0530

    2nd time added the command line

--- GitSampleProject/src/GitSampleProject/GitSample.java
+++ GitSampleProject/src/GitSampleProject/GitSample.java
@@ -4,7 +4,8 @@ public class GitSample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Frist Project about Git & GitHub");
+		/*add the Command Line*/
+				System.out.println("Frist Project about Git & GitHub");
 
 	}
 
